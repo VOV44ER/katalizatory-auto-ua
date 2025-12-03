e715790c7217d161ef6f1d432d4678f1af4f8070
Volodymyr Alputov <alputov1998@gmail.com>
2025-12-03 13:28:20 +0200

    Fix hero gradient overlay blocking submit button on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -246,7 +246,7 @@ export const Hero = () => {
       </div>
 
       {/* Decorative Elements */ }
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent z-10" />
+      <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-background to-transparent z-0" />
     </section>
   );
 };
